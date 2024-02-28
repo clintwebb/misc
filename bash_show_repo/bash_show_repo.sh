5e32ab72557c142d6b1dc270f798d85b4331a492
clintwebb <98885217+clintwebb@users.noreply.github.com>
2024-02-28 09:58:16 +0800

    Update bash_show_repo.sh
    Added some extra commentary, and since assuming everyone these days using Bash 4.0 or higher, have simplified the PROMPT_SSHAGENT check.

--- bash_show_repo/bash_show_repo.sh
+++ bash_show_repo/bash_show_repo.sh
@@ -1,13 +1,22 @@
 # Copyright 2024, Clinton Webb
 # This script can be referenced in .bashrc or .bash_profile which will set the Prompt.
 #
-# Example:
+# Example View:
 # webbcl@server1-1 ~/work/clintwebb/misc (main)$
 #
-#  This is showing the current username and server, followed by the current directory and the current branch if the current directory is part of a git repo
-#  It can also show some **** if the SSH agent isn't active.  This is very likely useful when using SSH protocol to sync.
-
-
+#  This is showing the current username and server, followed by the current directory 
+#  and the current branch if the current directory is part of a git repo
+#
+#  It can also show some **** if the SSH agent isn't active.  
+#  This is very likely useful when using SSH protocol to sync.
+#
+# Examples for .bashrc:
+#   # This will show the current branch if in a git repo (but will not show warning if ssh-agent not set)
+#   source ~/work/clintwebb/misc/bash_show_repo/bash_show_repo.sh
+#
+#   # This will show '****' in the prompt if ssh-agent not set (if set, it will not show anything extra)
+#   PROMPT_SSHAGENT=y
+#   source ~/work/clintwebb/misc/bash_show_repo/bash_show_repo.sh
 
 function parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -28,7 +37,7 @@ function parse_git_branch_sshagent() {
     fi
   fi
 }
-if [[ $PROMPT_SSHAGENT =~ ^(y|Y)$ ]]; then
+if [[ "${PROMPT_SSHAGENT,,} =~ ^(y(es)|1|true)$ ]]; then
   export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch_sshagent)\[\e[00m\]$ "
 else
   export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
