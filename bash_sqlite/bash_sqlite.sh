ad2b2c8800a1fc35ad5dddfe198c5c6c99c33744
clintwebb <98885217+clintwebb@users.noreply.github.com>
2024-03-10 17:19:48 +0800

    Update bash_sqlite.sh

--- bash_sqlite/bash_sqlite.sh
+++ bash_sqlite/bash_sqlite.sh
@@ -9,11 +9,25 @@
 # Pre-setting DB_FILE to something else is recommended, but if none is specified, it will choose a default filename.
 DB_FILE=${DB_FILE:-sqlite.db}
 
+# This query function will allow us to perform an operation, but wait for 20 seconds if the file is locked.
+# without a setting like this, if there is another operation on the file occuring at the same time, it will fail. 
+function query() {
+  [[ $SQL_DEBUG -gt 1 ]] && echo "$1" >&2
+  sqlite3 -init <(echo ".timeout 20000") $DB_FILE "$1" 2>/dev/null
+}
+
+function query_ro() {
+  [[ $SQL_DEBUG -gt 1 ]] && echo "$1" >&2
+  sqlite3 -readonly -init <(echo ".timeout 20000") $DB_FILE "$1" 2>/dev/null
+}
+
+
 
 # This query function will allow us to perform an operation.  
-function query() {
-
-  [[ $DEBUG -gt 1 ]] && echo "$1" >&2
+# This was modified from the previous version, because it wasn't working quite right on an example legacy redhat7 server (it kept on outputting an extra blank line)
+# still investigating.
+function query2() {
+  [[ $SQL_DEBUG -gt 1 ]] && echo "$1" >&2
 
   if [[ -e .init.sql ]]; then
     sqlite3 -init .init.sql $DB_FILE "$1" 2>/dev/null|tail -n +2
