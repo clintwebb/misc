92c97f0fcb5cf6e3d728d6e2576d80ec6ba849a1
clintwebb <98885217+clintwebb@users.noreply.github.com>
2024-03-19 09:37:38 +0800

    Update common_aliases.sh

--- bash/aliases/common_aliases.sh
+++ bash/aliases/common_aliases.sh
@@ -2,4 +2,4 @@ HISTCONTROL=ignoreboth
 function hc() { history -c && history -w; }
 function sshk() { [[ -n $1 ]] && sed -i ${1}d ~/.ssh/known_hosts; }
 function sudos { while true; do sudo sleep 60; done }
-function ddtmp { [[ ! -d $HOME/tmp]] && mkdir $HOME/tmp; [[ ! -d $HOME/tmp/$(date +%F) ]] && mkdir $HOME/tmp/$(date +%F); pushd $HOME/tmp/$(date +%F); }
+function ddtmp { [[ ! -d $HOME/tmp ]] && mkdir $HOME/tmp; [[ ! -d $HOME/tmp/$(date +%F) ]] && mkdir $HOME/tmp/$(date +%F); pushd $HOME/tmp/$(date +%F); }
